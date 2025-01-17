bea13748f542c15b985afaf347bb460d53306a33
chaeun kim <cocodata12@gmail.com>
2025-01-17 15:38:06 +0900

    sql 연습, 그리디 예제

--- hackers-rank/sql/SELECT_JOIN3.sql
+++ hackers-rank/sql/SELECT_JOIN3.sql
@@ -23,3 +23,12 @@ SELECT CASE WHEN b.grade >= 8 THEN a.name
 FROM Students a, Grades b
 WHERE (a.marks between b.min_mark and max_mark)
 ORDER BY grade DESC, name ASC, marks ASC;
+
+-- Ollivander's Inventory
+SELECT w1.id, wp1.age, w1.coins_needed, w1.power
+FROM wands w1 INNER JOIN wands_property wp1 ON (w1.code = wp1.code)
+WHERE wp1.is_evil != 1
+AND coins_needed = (SELECT min(coins_needed)
+                    FROM wands w2 INNER JOIN wands_property wp2 ON (w2.code = wp2.code)
+                    WHERE w1.power = w2.power AND wp1.age = wp2.age)
+ORDER BY power DESC, age DESC;
--- this-is-a-coding-test/greedy/ch11/Q1_6_Repeat.py
+++ this-is-a-coding-test/greedy/ch11/Q1_6_Repeat.py
@@ -1,5 +1,8 @@
 # 그리디 예제 복습
 
+from itertools import combinations
+import heapq
+
 # 1, 모험가 길드
 '''
 5
@@ -61,7 +64,49 @@ def cantBeMade():
 
     print(target)
 
+# 5, 볼링공 고르기
+'''
+5 3
+1 3 2 3 2
+
+8 5
+1 5 4 3 2 4 5 2
+'''
+def bowlingBall():
+    n, m = map(int, input().split())
+    balls = list(map(int, input().split()))
+    combi = list(combinations(balls, 2))
+    for a, b in combi:
+        if a == b: combi.remove((a, b))
+
+    print(len(combi))
+
+# 6, 무지의 먹방 라이브
+# https://programmers.co.kr/learn/courses/30/lessons/42891
+def solution(food_times, k):
+    if sum(food_times) <= k: return -1
+    q = []
+    for i in range(len(food_times)):
+        heapq.heappush(q, (food_times[i], i + 1))
+
+    used_time = 0
+    prev = 0
+    length = len(food_times)
+
+    # 사용한 시간 + 이번 음식을 먹는데 덜리는 시간이 k 보다 작을 때 까지 반복
+    while used_time + ((q[0][0] - prev) * length)<= k:
+        now = heapq.heappop(q)[0] # 현재 먹어야 할 음식, 걸리는 시간
+        used_time += (now - prev) * length # 현재 음식을 다 먹는 경우 걸리는 시간: (음식 먹는데 걸리는 시간 - 이전에 보낸 시간(먹은 시간)) * 음식의 개수
+        length -= 1 # 한 가지 음식을 다 먹었으므로, 총 음식의 갯수 -1
+        prev = now # 음식을 먹는데 소모한 시간 업데이트
+
+    # 남은 음식 중 몇 번째 음식인지 확인 후 출력
+    result = sorted(q, key=lambda x: x[1]) # 음식 번호 기준으로 정렬
+    target = (k - used_time) % length # 남은 시간 = (k - 사용한 시간) % 남은 음식 갯수
+    # 큐에서 남은 시간 번째 요소가 다음으로 먹어야 할 음식.
+    return result[target][1]
 
 
 if __name__ == "__main__":
-    cantBeMade()
+    food_times = [3, 1, 2]
+    k = 5
