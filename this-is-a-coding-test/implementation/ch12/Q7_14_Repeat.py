2d1b1db1e0374434e76134ef9efb3da9610105db
chaeun kim <cocodata12@gmail.com>
2025-02-04 13:59:20 +0900

    구현 예제

--- this-is-a-coding-test/implementation/ch12/Q7_14_Repeat.py
+++ this-is-a-coding-test/implementation/ch12/Q7_14_Repeat.py
@@ -1,4 +1,10 @@
 
+def print_matrix(matrix):
+    print('**')
+    for m in matrix:
+        print(m)
+    print('**')
+
 # 럭키스트레이트
 # 123402, 7755
 def luckyStraight(n):
@@ -49,5 +55,56 @@ def stringCompression(s):
             answer = compression
     return answer
 
+# 자물쇠와 열쇠
+# ToDo - 90도 회전, 자물쇠 확장, 열 수 있는지 판단
+def turn90(key):
+    m = len(key)
+    result = [[] for _ in range(m)]
+    for i in range(m):
+        for j in range(m-1, -1, -1):
+            result[i].append(key[j][i])
+    return result
+
+def expendLock(lock):
+    n = len(lock)
+    result = [[0] * (n * 3) for _ in range(n * 3)] # 3배 확장
+    for i in range(n):
+        for j in range(n):
+            result[i + n][j + n] += lock[i][j]
+    return result
+
+def isOpen(expended_lock, n):
+    for i in range(n):
+        for j in range(n):
+            if expended_lock[i + n][j + n] != 1:
+                return False
+    return True
+
+def lockAndKey(lock, key):
+    expended_lock = expendLock(lock)
+    n = len(lock)
+    m = len(key)
+
+    for _ in range(4):
+        key = turn90(key) # 열쇠 회전
+        # 자물쇠
+        for i in range(n * 2):
+            for j in range(n * 2):
+                #열쇠
+                for ki in range(m):
+                    for kj in range(m):
+                        expended_lock[i + ki][j + kj] += key[ki][kj]
+
+                if isOpen(expended_lock, n):
+                    return True
+
+                # 원상복구
+                for ki in range(m):
+                    for kj in range(m):
+                        expended_lock[i + ki][j + kj] -= key[ki][kj]
+    return False
+
 if __name__ == "__main__":
-    pass
+    key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
+    lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+    print(lockAndKey(lock=lock, key=key))
